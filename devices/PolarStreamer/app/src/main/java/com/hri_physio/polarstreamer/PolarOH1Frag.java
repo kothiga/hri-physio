95ac89d8cb738cd71f8a5866b19f87406af39a6d
fangxiaoxiao-iris <x37fang@edu.uwaterloo.ca>
2020-10-02 00:47:06 -0400

    Added enter device id button

--- devices/PolarStreamer/app/src/main/java/com/hri_physio/polarstreamer/PolarOH1Frag.java
+++ devices/PolarStreamer/app/src/main/java/com/hri_physio/polarstreamer/PolarOH1Frag.java
@@ -12,6 +12,6 @@ public class PolarOH1Frag extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.polar_oh1_frag, container, false);
     }
 }
