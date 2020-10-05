80e60e2daf4133ccd4e67d07941d371a83170294
fangxiaoxiao-iris <x37fang@edu.uwaterloo.ca>
2020-10-05 15:11:20 -0400

    Moved function in main to its own fragment

--- devices/PolarStreamer/app/src/main/java/com/hri_physio/polarstreamer/MainActivity.java
+++ devices/PolarStreamer/app/src/main/java/com/hri_physio/polarstreamer/MainActivity.java
@@ -1,31 +1,19 @@
 package com.hri_physio.polarstreamer;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
-
-import androidx.appcompat.app.AlertDialog;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.text.InputType;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-
 import com.hri_physio.polarstreamer.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
     private String DEVICE_ID;
-    SharedPreferences sharedPreferences;
+    public SharedPreferences sharedPreferences;
     private String sharedPrefsKey = "polar_device_id";
 
     @Override
@@ -47,37 +35,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
 
-    public void onClickChangeID(View view) {
-        showDialog(view);
-    }
 
-    public void showDialog(View view){
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this, R.style.PolarTheme);
-        dialog.setTitle("Enter your Polar device's ID");
-
-        View viewInflated = LayoutInflater.from(getApplicationContext()).inflate(R.layout.device_id_dialog_layout,(ViewGroup) view.getRootView() , false);
-
-        final EditText input = viewInflated.findViewById(R.id.input);
-        input.setInputType(InputType.TYPE_CLASS_TEXT);
-        dialog.setView(viewInflated);
-
-        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                DEVICE_ID = input.getText().toString();
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString(sharedPrefsKey, DEVICE_ID);
-                editor.apply();
-            }
-        });
-        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-        dialog.show();
     }
 }
--- devices/PolarStreamer/app/src/main/java/com/hri_physio/polarstreamer/PolarOH1Frag.java
+++ devices/PolarStreamer/app/src/main/java/com/hri_physio/polarstreamer/PolarOH1Frag.java
@@ -1,17 +1,69 @@
 package com.hri_physio.polarstreamer;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
-
 public class PolarOH1Frag extends Fragment {
+    private String DEVICE_ID;
+    public SharedPreferences sharedPreferences;
+    private String sharedPrefsKey = "polar_device_id";
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.polar_oh1_frag, container, false);
+        View view = inflater.inflate(R.layout.polar_oh1_frag, container, false);
+        sharedPreferences = this.getActivity().getPreferences(Context.MODE_PRIVATE);
+
+        Button enterIDButton = (Button) view.findViewById(R.id.buttonSetID);
+        enterIDButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickChangeID(view);
+            }
+        });
+
+        return view;
+    }
+    public void onClickChangeID(View view) {
+        showDialog(view);
+    }
+
+    public void showDialog(View view){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this.getContext(), R.style.PolarTheme);
+        dialog.setTitle("Enter your Polar OH1 device's ID");
+
+        View viewInflated = LayoutInflater.from(getActivity().getApplicationContext()).inflate(R.layout.device_id_dialog_layout,(ViewGroup) view.getRootView() , false);
+
+        final EditText input = viewInflated.findViewById(R.id.input);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        dialog.setView(viewInflated);
+
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                DEVICE_ID = input.getText().toString();
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(sharedPrefsKey, DEVICE_ID);
+                editor.apply();
+            }
+        });
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        dialog.show();
     }
 }
